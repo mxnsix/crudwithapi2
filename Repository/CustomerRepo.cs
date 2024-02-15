24a877b435be78232e6b422394b217bd12c1eff2
mxnsix <ms4134559@gmail.com>
2024-02-15 10:33:38 +0530

    crudusingc

---
+++ Repository/CustomerRepo.cs
@@ -0,0 +1,129 @@
+﻿using api2.Repository.interfaces;
+using System.Data.SqlClient;
+using api2.Models;
+
+namespace api2.Repository
+{
+    public class CustomerRepo:ICustomerRepo
+    {
+        readonly string connectionString = "";
+        public CustomerRepo()
+        {
+            connectionString = "Data Source=APINP-ELPTKUXQY\\SQLEXPRESS;Initial Catalog=chubb;User ID=tap2023;Password=tap2023;Encrypt=False";
+        }
+
+        public Customer GetCustomerById(string id)
+        {
+            Customer c = null;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = $"SELECT CustomerId, CompanyName, Address, Contactname FROM Customers WHERE CustomerID = '{id}'";
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = query;
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    c = new Customer();
+                    c.CustomerId = dr["CustomerId"].ToString();
+                    c.CompanyName = dr["CompanyName"].ToString();
+                    c.ContactName = dr["ContactName"].ToString();
+                    c.Address = dr["Address"].ToString();
+                }
+            }
+            return c;
+        }
+
+
+        public List<string> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void CreateCustomer(Customer customer)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Customers (CustomerId, CompanyName, ContactName, Address) VALUES (@CustomerId, @CompanyName, @ContactName, @Address)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@CustomerId", customer.CustomerId);
+                cmd.Parameters.AddWithValue("@CompanyName", customer.CompanyName);
+                cmd.Parameters.AddWithValue("@ContactName", customer.ContactName);
+                cmd.Parameters.AddWithValue("@Address", customer.Address);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE Customers SET CompanyName = @CompanyName, ContactName = @ContactName, Address = @Address WHERE CustomerId = @CustomerId";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@CustomerId", customer.CustomerId);
+                cmd.Parameters.AddWithValue("@CompanyName", customer.CompanyName);
+                cmd.Parameters.AddWithValue("@ContactName", customer.ContactName);
+                cmd.Parameters.AddWithValue("@Address", customer.Address);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteCustomer(string id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string deleteOrderDetailsQuery = "DELETE FROM [Order Details] WHERE OrderID IN (SELECT OrderID FROM Orders WHERE CustomerID = @Id)";
+                SqlCommand deleteOrderDetailsCmd = con.CreateCommand();
+                deleteOrderDetailsCmd.CommandText = deleteOrderDetailsQuery;
+                deleteOrderDetailsCmd.Parameters.AddWithValue("@Id", id);
+                con.Open();
+                deleteOrderDetailsCmd.ExecuteNonQuery();
+
+                string deleteOrdersQuery = "DELETE FROM Orders WHERE CustomerID = @Id";
+                SqlCommand deleteOrdersCmd = con.CreateCommand();
+                deleteOrdersCmd.CommandText = deleteOrdersQuery;
+                deleteOrdersCmd.Parameters.AddWithValue("@Id", id);
+                deleteOrdersCmd.ExecuteNonQuery();
+
+                string deleteCustomerQuery = "DELETE FROM Customers WHERE CustomerID = @Id";
+                SqlCommand deleteCustomerCmd = con.CreateCommand();
+                deleteCustomerCmd.CommandText = deleteCustomerQuery;
+                deleteCustomerCmd.Parameters.AddWithValue("@Id", id);
+                deleteCustomerCmd.ExecuteNonQuery();
+            }
+        }
+
+        public List<Customer> SearchCustomers(string searchCriteria)
+        {
+            List<Customer> customers = new List<Customer>();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "SELECT CustomerId, CompanyName, Address, Contactname FROM Customers WHERE CompanyName LIKE @SearchCriteria OR ContactName LIKE @SearchCriteria OR Address LIKE @SearchCriteria";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@SearchCriteria", "%" + searchCriteria + "%");
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Customer c = new Customer();
+                    c.CustomerId = dr["CustomerId"].ToString();
+                    c.CompanyName = dr["CompanyName"].ToString();
+                    c.ContactName = dr["ContactName"].ToString();
+                    c.Address = dr["Address"].ToString();
+                    customers.Add(c);
+                }
+            }
+
+            return customers;
+        }
+        
+
+
+    }
+}
